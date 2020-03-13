657632fdec806248e2106a432377aa7667740885
github username <github email address>
2020-03-13 12:37:57 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,39 @@
 // your Bomb code here!
+import React from "react"
+import { render } from "enzyme"
+
+class Bomb extends React.Component {
+    
+    constructor(props) {
+        super() 
+        this.state = {secondsLeft:props.initialCount}
+    }
+
+    startTimer = () => {
+        let b = document.querySelector("button")
+        b.style = {display:'none'}
+        const tick = setInterval(()=>{ 
+            let newSecs = --this.state.secondsLeft
+            this.state.secondsLeft >= 0? 
+            this.setState({secondsLeft:newSecs}):clearInterval(tick)}
+        ,1000)
+    }
+    
+    render() {
+    console.log(this.state.secondsLeft)
+    // {this.state.secondsLeft == 0? a:b}    
+    return (
+            <div>
+                {this.state.secondsLeft > 0? 
+                <p>{this.state.secondsLeft} seconds left before I go boom!</p>
+                :<p>Boom!</p>}
+                {/* <button onClick={this.startTimer}>DON'T PRESS THIS BUTTON</button> */}
+            </div>
+        ) 
+    }
+
+    
+}
+
+// your ImageSlider code here!
+export default Bomb
